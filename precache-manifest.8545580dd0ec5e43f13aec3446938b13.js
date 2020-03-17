ce6b777a3e0fd4a6993f2670bf9c849613569797
adeelseraj <61334305+adeelseraj@users.noreply.github.com>
2020-03-17 23:03:26 +0500

    Updates

---
+++ precache-manifest.8545580dd0ec5e43f13aec3446938b13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa11b8536a2af45263fdc8acf9b021f",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "7b739d4fa56e8233c69c",
+    "url": "/to-do-app/static/css/main.ec9f5bae.chunk.css"
+  },
+  {
+    "revision": "31fc0b138878aba74626",
+    "url": "/to-do-app/static/js/2.da5a1a2b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/to-do-app/static/js/2.da5a1a2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b739d4fa56e8233c69c",
+    "url": "/to-do-app/static/js/main.3bc2cb9d.chunk.js"
+  },
+  {
+    "revision": "3b10be5902a13e691974",
+    "url": "/to-do-app/static/js/runtime-main.bd605cde.js"
+  }
+]);
